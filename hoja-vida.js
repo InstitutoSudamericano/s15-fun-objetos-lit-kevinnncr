63a4b2c454f1cdd1acef6ba2c714a6b4ccdfa8dc
Pablo Lucero <116099973+Lcandres16@users.noreply.github.com>
2023-08-04 10:35:16 -0500

    tarea

--- hoja-vida.js
+++ hoja-vida.js
@@ -1,2 +1,16 @@
-//Crear variables con diferentes tipos de datos (number, string, array, objetos literales) para almacenar información de una persona, 
-//pasar estos como parámetros de una función, que imprimirá en pantalla usando un template string con backcstick (``).
+
+let fullname = "kevin criollo";
+let age = 40;
+let skills = ["deportista", "Desarrollador de software", "diseñador"];
+let address = { mainStreet: "monay", secondaryStreet: "valle"};
+
+function showCurriculum(name, age, skills, address) {
+    let formatedSkills = skills.join('\n\t');
+    let formatedAddress = `${address.mainStreet}\t${address.secondaryStreet}\t${address.streetNumber}`;
+    let message = `Name: ${name}\nAge: ${age}\nSkills:\n\t${formatedSkills}\nAddress:\n\t${formatedAddress}`;
+    return message;
+}
+
+let hojadeVida = showCurriculum(fullname, age, skills, address);
+
+console.log(hojadeVida);
--- validar-vacuna.js
+++ validar-vacuna.js
@@ -1,4 +1,12 @@
-//Crear una función que reciba un objeto literal con nombre y edad de una persona, 
-//la función debe validar si la edad aceptado para recibir la vacuna que debe ser 
-//para menores a 5 anos o mayor a 65. La salida de la función es un string de
-//aceptado y no aceptado.
+let person = {name:"Kevin", age:20}
+
+function validarvacuna(literalObject){
+    if (literalObject.age<5 || literalObject.age>65){
+        return `${literalObject.name} es aceptado`
+    }
+    return `${literalObject.name} no es aceptado`
+}
+
+let result = validarvacuna(person);
+
+alert(result);
